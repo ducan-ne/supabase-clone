ffd576a447e7da33bd67fa9253d4768dca86338e
An <ancmsvn@gmail.com>
2022-11-27 17:37:07 +0700

    first commit

---
+++ public/The Open Source Firebase Alternative _ Supabase_files/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fpartners\u002Fexperts","\u002Fpartners\u002Fintegrations","\u002Fchangelog","\u002Fblog\u002Ftags\u002F[tag]","\u002Fpartners\u002F[slug]","\u002Falternatives\u002F[slug]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
